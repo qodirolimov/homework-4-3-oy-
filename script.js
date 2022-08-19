8d7d0702a631bf8d2471a95630f8b9b7021d61e3
qodirolimov <qodirolimov88@gmail.com>
2022-08-19 21:53:43 +0500

    next

---
+++ script.js
@@ -0,0 +1,248 @@
+"use strict"
+
+
+
+// ============================ 1-masala
+    
+   let result2=document.querySelector('#result2');
+   let num3=document.getElementById('num3');
+
+    
+   let daraja3=()=>{
+      return result2.textContent=num3.value**3;
+    }
+      
+
+
+      let del2=()=>{
+         return result2.textContent="",
+         num3.value=""
+      
+       }
+
+
+
+
+// ============================ 2-masala
+
+    let rusult3=document.querySelector('#result3');
+    let num4=document.getElementById('num4');
+    let num5=document.getElementById('num5');
+
+    let add2=()=>{
+      return result3.textContent=num4.value*1 + num5.value*1; 
+    }
+
+
+
+    let del3=()=>{
+      return result3.textContent="",
+      num4.value="",
+      num5.value=""
+   
+    }
+
+
+
+// ============================ 3-masala
+
+    let rusult4=document.querySelector('#result4');
+    let num6=document.getElementById('num6');
+    let num7=document.getElementById('num7');
+
+    let minus2=()=>{
+      return result4.textContent=num6.value*1 - num7.value*1; 
+    }
+
+
+
+    let del4=()=>{
+      return result4.textContent="",
+      num6.value="",
+      num7.value=""
+   
+    }
+
+    
+    
+
+
+    // ============================ 4-masala
+
+    let result5=document.querySelector('#result5');
+    let num8=document.getElementById('num8');
+ 
+     
+    let daraja2=()=>{
+       return result5.textContent=num8.value**2;
+     }
+       
+     let darajathird=()=>{
+      return result5.textContent=num8.value**3;
+    }
+      
+
+    let daraja4=()=>{
+      return result5.textContent=num8.value**4;
+    }
+      
+ 
+ 
+       let del5=()=>{
+          return result5.textContent="",
+          num8.value=""
+       
+        }
+
+
+
+
+
+    // ============================ 7-masala
+
+    let num11=document.querySelector('#num11');
+    
+
+
+    let add3=()=>{
+      return num11.textContent=num11.value++; 
+    }
+
+    let minus3=()=>{
+      return num11.textContent=num11.value--; 
+    }
+
+
+
+    let del7=()=>{
+      return num11.textContent="",
+      num11.value=""
+    
+    }
+
+
+
+
+  
+    // ============================ 8-masala
+
+
+        let rusult6=document.querySelector('#result6');
+        let num9=document.getElementById('num9');
+        let num10=document.getElementById('num10');
+    
+        let devide2=()=>{
+          return result6.textContent=num9.value*1 / num10.value*1; 
+        }
+    
+    
+    
+        let del6=()=>{
+          return result6.textContent="",
+          num9.value="",
+          num10.value=""
+       
+        }
+
+
+
+
+           // ============================ 9-masala
+
+            const result=document.querySelector('#result');
+            const num1=document.getElementById('num1');
+           const num2=document.getElementById('num2');
+
+
+            const add=()=>{
+            return result.textContent=num1.value * 1 + num2.value*1;
+            }
+
+            const minus=()=>{
+            return result.textContent=num1.value * 1 - num2.value*1;
+              }
+
+             const multiple=()=>{
+             return result.textContent=num1.value * 1 * num2.value*1;
+             }
+             const devide=()=>{
+   
+              return result.textContent=num1.value * 1 / num2.value*1;
+              }
+              const del=()=>{
+               return result.textContent="",
+              num1.value="",
+              num2.value=""
+               }
+        
+
+
+
+
+          // ============================ 10-masala
+
+    let num12=document.querySelector('#num12');
+    
+
+
+    let multiple2=()=>{
+      return result7.textContent=num12.value*1; 
+    }
+
+    let multiple3=()=>{
+
+       return result7.textContent=num12.value*2; 
+      }
+
+      let multiple4=()=>{
+        return result7.textContent=num12.value*3; 
+      }
+  
+      let multiple5=()=>{
+  
+         return result7.textContent=num12.value*4; 
+        }
+
+        
+        let multiple6=()=>{
+          return result7.textContent=num12.value*5; 
+        }
+    
+        let multiple7=()=>{
+    
+           return result7.textContent=num12.value*6; 
+          }
+
+          
+          let multiple8=()=>{
+            return result7.textContent=num12.value*7; 
+          }
+      
+          let multiple9=()=>{
+      
+             return result7.textContent=num12.value*8; 
+            }
+      
+
+            
+          let multiple10=()=>{
+      
+            return result7.textContent=num12.value*9; 
+           }
+     
+
+           let multiple11=()=>{
+      
+            return result7.textContent=num12.value*10; 
+           }
+     
+             let del8=()=>{
+             return result7.textContent="",
+            result7.value=""
+          
+            }
+
+                     
+
+        
+
